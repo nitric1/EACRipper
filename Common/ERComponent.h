ca87316e2b1fa9fd7a4d757bf7f1a33ec5cf8577
Nitric <mail@nitric.me>
2010-03-18 18:50:43 +0900

    Working on component.

--- Common/ERComponent.h
+++ Common/ERComponent.h
@@ -17,13 +17,6 @@ class ERServiceFactory;
 
 #include <cinttypes>
 
-#if defined(_WINDOWS) && defined(_USRDLL)
-#include <windows.h>
-
-extern "C" __declspec(dllexport) void initComponent(IERApplication *);
-extern "C" __declspec(dllexport) void uninitComponent();
-#endif
-
 class ERApplicationInfo
 {
 private:
@@ -38,6 +31,8 @@ private:
 public:
 	static ERApplicationInfo &instance() { ERApplicationInfo theInstance; return theInstance; }
 	IERApplication &getApp() { return theApp; }
+
+	friend void initComponent(IERApplication *);
 };
 
 template<typename T>
@@ -51,22 +46,57 @@ public:
 	T *operator ->() { return ptr; }
 };
 
+namespace ComponentGlobal
+{
+	extern const wchar_t *componentName;
+	extern const wchar_t *componentVersion;
+
+	extern const IERComponentEntrypoint *componentEntry;
+}
+
 template<typename T>
 class ERComponentEntrypointFactory
 {
+public:
+	ERComponentEntrypointFactory()
+	{
+		ComponentGlobal::componentEntry = new T;
+	}
+
+	~ERComponentEntrypointFactory()
+	{
+		delete ComponentGlobal::componentEntry;
+	}
 };
 
-#define DECLARE_COMPONENT(name, version, entry) \
-	class ERComponentInfo : public IERComponentInfo \
-	{ \
-		virtual const wchar_t *getSDKVersion() { return EACRIPPER_COMPONENT_SDK_VERSION; } \
-		virtual const wchar_t *getComponentName() { return name; } \
-		virtual const wchar_t *getComponentVersion() { return version; } \
-	}; \
-	extern "C" __declspec(dllexport) void initComponent(IERApplication *) \
-	{ \
-		; \
-	} \
-	extern "C" __declspec(dllexport) void uninitComponent() \
-	{ \
+class ERComponentInfo : public IERComponentInfo
+{
+public:
+	virtual const wchar_t *getSDKVersion() { return EACRIPPER_COMPONENT_SDK_VERSION; }
+	virtual const wchar_t *getComponentName() { return ComponentGlobal::componentName; }
+	virtual const wchar_t *getComponentVersion() { return ComponentGlobal::componentVersion; }
+};
+
+#define DECLARE_COMPONENT(name, version) \
+	namespace ComponentGlobal { \
+		const wchar_t *componentName = name; \
+		const wchar_t *componentVersion = version; \
+		const IERComponentEntrypoint *componentEntry = NULL; \
 	}
+
+#if defined(_WINDOWS) && defined(_USRDLL)
+#include <windows.h>
+
+extern "C" __declspec(dllexport) void initComponent(IERApplication *app)
+{
+	ERApplicationInfo::instance().setApp(app);
+	if(ComponentGlobal::componentEntry != NULL)
+		ComponentGlobal::componentEntry->onInit();
+}
+
+extern "C" __declspec(dllexport) void uninitComponent()
+{
+	if(ComponentGlobal::componentEntry != NULL)
+		ComponentGlobal::componentEntry->onUninit();
+}
+#endif
